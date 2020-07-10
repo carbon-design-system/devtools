c793e8965cf3dccf22c85161f8e4fdbcd97ea710
James Dow <james.dow@us.ibm.com>
2020-07-10 15:39:29 -0500

    updated package.json versions to latest

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,5 @@
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
