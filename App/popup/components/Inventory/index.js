73d8706df82eb85442b9eae5259f0ca4d7c67d52
James Dow <james.dow@us.ibm.com>
2020-07-28 09:31:49 -0500

    adding skeleton placeholder for beta

--- App/popup/components/Inventory/index.js
+++ App/popup/components/Inventory/index.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { settings } from 'carbon-components';
-import { ToggleSmall } from 'carbon-components-react';
-import { ComingSoon } from '../';
+import { Accordion, AccordionItem, AccordionSkeleton } from 'carbon-components-react';
 
 const { prefix } = settings;
 
 function Inventory () {
-    return (
-        <ComingSoon />
-    );
+    return loading();
+}
+
+function loading () {
+    return <AccordionSkeleton align="start" open={false} count={3} />
 }
 
 export { Inventory };
