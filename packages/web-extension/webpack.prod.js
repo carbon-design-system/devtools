179e7d29cb6d70bdf576add3616a88c314436e81
James Dow <james.dow@us.ibm.com>
2020-10-24 11:42:50 -0500

    🎉 automated zip file compilation

--- packages/web-extension/webpack.prod.js
+++ packages/web-extension/webpack.prod.js
@@ -1,7 +1,9 @@
 const TerserPlugin = require('terser-webpack-plugin');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const ZipPlugin = require('zip-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const package = require('./package');
 
 module.exports = merge(common, {
     mode: 'production',
@@ -11,5 +13,10 @@ module.exports = merge(common, {
             new TerserPlugin({}),
             new OptimizeCSSAssetsPlugin({})
         ]
-    }
+    },
+    plugins: [
+        new ZipPlugin({
+            filename: package.name + '-' + package.version + '' + '.zip'
+        })
+    ]
 });
