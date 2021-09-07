96cf4cad0131b450ea7052ee64382adc21fc0c33
Simon Finney <SIMONFIN@ie.ibm.com>
2021-09-07 17:31:38 +0100

    feat(cloud-cognitive): add prefix selector

--- packages/web-extension/src/globals/prefixSelectors.js
+++ packages/web-extension/src/globals/prefixSelectors.js
@@ -1,16 +1,30 @@
-import { carbonPrefix } from './';
+import { pkg } from '@carbon/ibm-cloud-cognitive';
 import { settings as dotcomSettings } from '@carbon/ibmdotcom-utilities';
 import { getComponentNamespace as getSecurityPrefix } from '@carbon/ibm-security/es/globals/namespace';
+
+import { carbonPrefix } from '.';
 
 const { stablePrefix: dotcomPrefix } = dotcomSettings;
 const securityPrefix = getSecurityPrefix('');
 const cloudPalPrefix = 'pal'; // static hardcoded
 
-const prefixSelectors =
-  `[class*="${carbonPrefix}--"], ` + // carbon
-  `[data-autoid*="${dotcomPrefix}--"], [data-auto-id*="${dotcomPrefix}--"], ` + // DDS
-  `[class*="${securityPrefix}"], ` + // security
-  `[class*="${cloudPalPrefix}--"]`; // pal
+const {
+  devtoolsAttribute: cloudCognitiveDevtoolsAttribute,
+  getDevtoolsId: getCloudCognitiveDevtoolsId,
+} = pkg;
+
+const prefixSelectors = [
+  `class*="${carbonPrefix}--"`, // Carbon
+  `${cloudCognitiveDevtoolsAttribute}*="${getCloudCognitiveDevtoolsId('')}"`, // Cloud & Cognitive — https://github.com/carbon-design-system/ibm-cloud-cognitive
+  `data-autoid*="${dotcomPrefix}--"`, // IBM.com
+  `data-auto-id*="${dotcomPrefix}--"`, // IBM.com
+  `class*="${securityPrefix}"`, // Security
+  `class*="${cloudPalPrefix}--"`, // Cloud PAL
+].reduce(
+  (prefixSelectors, prefixSelector, index) =>
+    `${index > 0 ? `${prefixSelectors}, ` : ''}[${prefixSelector}]`,
+  ''
+);
 
 export {
   prefixSelectors,
