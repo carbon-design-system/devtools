4442c1d7e342f2e3b631c58a3526d2c3181ca85b
James Dow <james.dow@us.ibm.com>
2021-09-08 02:40:54 -0500

    chore: configured to build static in out

--- packages/demo-site/next.config.js
+++ packages/demo-site/next.config.js
@@ -4,7 +4,7 @@ const withSass = require('@zeit/next-sass');
 const rtlcss = require('rtlcss');
 
 module.exports = withSass({
-  assetPrefix: process.env.ASSET_PREFIX || '/random-ibm/',
+  assetPrefix: process.env.ASSET_PREFIX || '/',
   basePath: process.env.BASE_PATH || '',
   env: {
     ALTLANG_ROOT_PATH: process.env.ALTLANG_ROOT_PATH || '/',
