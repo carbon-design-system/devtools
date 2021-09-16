93af2f8abbecb2ce4ed9f6790e72eb109f18b319
James Dow <james.dow@us.ibm.com>
2021-09-16 09:37:29 -0500

    chore: renamed zip file

--- packages/web-extension/webpack.prod.js
+++ packages/web-extension/webpack.prod.js
@@ -3,7 +3,6 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const ZipPlugin = require('zip-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const packageJSON = require('./package');
 
 module.exports = merge(common, {
   mode: 'production',
@@ -13,7 +12,7 @@ module.exports = merge(common, {
   },
   plugins: [
     new ZipPlugin({
-      filename: packageJSON.name + '-' + packageJSON.version + '' + '.zip',
+      filename: 'web-extension.zip',
     }),
   ],
 });
