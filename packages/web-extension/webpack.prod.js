294af1d2940717ea4793a113c4bb8d50c3d8c62c
Zhen Wang <zhen.wang1@ibm.com>
2026-05-28 11:13:26 +0200

    chore: update dev deps

--- packages/web-extension/webpack.prod.js
+++ packages/web-extension/webpack.prod.js
@@ -2,13 +2,13 @@ const TerserPlugin = require('terser-webpack-plugin');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const ZipPlugin = require('zip-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
   optimization: {
     minimize: true,
-    minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    minimizer: [new TerserPlugin({}), new CssMinimizerPlugin({})],
   },
   plugins: [
     new ZipPlugin({
